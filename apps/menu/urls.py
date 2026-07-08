02ac3d04baf481c2d571e27d4fd2fd8af369467b
badiahalotaibi-oss <badiahalotaibi.com>
2026-07-09 01:31:09 +0300

    Restore stable routes and old font

--- apps/menu/urls.py
+++ apps/menu/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-
 from . import views
 
-
-app_name = 'menu'
+app_name = "menu"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('cashier/menu-screen/', views.home, name='cashier_menu_screen'),
-    path('category/<slug:slug>/', views.category_detail, name='category_detail'),
+    path("", views.home, name="home"),
+    path("category/<slug:slug>/", views.category_detail, name="category_detail"),
 ]
